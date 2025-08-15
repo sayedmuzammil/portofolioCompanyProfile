ccf514cb5148e54d216e669a138f0b311681c3d0
sayedmuzammil <smuzammil87@gmail.com>
2025-08-15 10:23:47 +0700

    update

---
+++ src/app/data/navigation-data.ts
@@ -0,0 +1,27 @@
+type navigationItem = {
+  label: string;
+  href: string;
+};
+
+export const navigationList: navigationItem[] = [
+  {
+    label: 'About',
+    href: '#about',
+  },
+  {
+    label: 'Services',
+    href: '#services',
+  },
+  {
+    label: 'Projects',
+    href: '#projects',
+  },
+  {
+    label: 'Testimonials',
+    href: '#testimonials',
+  },
+  {
+    label: 'FAQ',
+    href: '#faq',
+  },
+];
---
+++ src/app/home/navbar.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Button>test</Button>
+    </div>
+  );
+};
+
+export default Navbar;
